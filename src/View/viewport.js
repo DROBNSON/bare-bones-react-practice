e5d5e1b355de3d255e96762626ea50f426ba6797
Deshaun Robinson <deshaun.robinson@outlook.com>
2025-02-09 06:59:22 -0500

    Added anchor tags and made genral styling changes

--- src/View/viewport.js
+++ src/View/viewport.js
@@ -1,16 +1,33 @@
 import React from "react";
 function Navbar(){
-    return(
-        <>
+    return (
+      <>
         <header>
-            <nav className="w-[90vw] h-16 flex mx-auto bg-gray-800 text-white">
-                <div class="left-navbar"></div>
-                <div class="centre-left-navbar"></div>
-                <div class="centre-right-navbar"></div>
-                <div class="right-navbar"></div>
-            </nav>
+          <nav className="w-[90vw] h-10 flex mx-auto bg-gray-800 text-white">
+            <div
+              class="left-navbar"
+              className="h-full flex-1  bg-primaryBlue p-4 flex-grow rounded-md"
+            >
+              <a href="www.google.com">Products</a>
+              <a href="www.google.com">Solutions</a>
+              <a href="www.google.com">Why Aspire</a>
+              <a href="www.google.com">Resources</a>
+            </div>
+            <div
+              class="centre-left-navbar"
+              className="h-full flex-1 bg-primaryBlue p-4 flex-growb rounded-md"
+            ></div>
+            <div
+              class="centre-right-navbar"
+              className="h-full flex-1  bg-primaryBlue p-4 flex-grow rounded-md"
+            ></div>
+            <div
+              class="right-navbar"
+              className="h-full flex-1  bg-primaryBlue p-4 flex-grow rounded-md"
+            ></div>
+          </nav>
         </header>
-        </>
+      </>
     );
 }
 
@@ -18,7 +35,7 @@ function Navbar(){
 export function Viewport() {
 
  return (
-   <div className="w-screen h-screen m-0 p-0 flex bg-blue-500">
+   <div className="w-screen h-screen m-0 p-0 block bg-primaryBlue">
     <Navbar></Navbar>
      <p>Hello to the world</p>
    </div>
